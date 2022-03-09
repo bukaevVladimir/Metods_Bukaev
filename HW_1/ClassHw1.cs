ee8a11aac198c8b7f98cd7204c7241b1f1b87128
Vladimir Bukaev <bukaevv0@gmail.com>
2022-03-09 16:50:21 +0300

    decision work (if)

---
+++ HW_1/ClassHw1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_1
+{
+    public class ClassHw1 // дз№1
+    {
+        public static double Qweqwe(double a, double b)  // 2 задача
+        {
+            return ((5 * a) + Math.Pow(b, 2)) / (b - a);
+        }
+
+        public static void Third(ref string s1, ref string s2)
+        {
+            string s3;
+            s3 = s1;
+            s1 = s2;
+            s2 = s3;
+        }
+
+        public static double LinearEquation(double a_4, double b_4, double c_4) //4 задача
+            {
+                return((c_4 - b_4) / a_4);
+            }
+
+        public static double ExecuteIf(double a_if, double b_if)// 1 задача из if
+        {
+           if(a_if > b_if)
+            {
+                return a_if + b_if;
+            }
+           if(a_if == b_if)
+            {
+                return a_if * b_if;
+            }
+            return a_if - b_if;
+        }
+    }
+}
--- HW_1/Program.cs
+++ HW_1/Program.cs
@@ -1,1 +1,38 @@
-﻿
+﻿using HW_1;
+
+// 2 задача 
+Console.Write("Enter the first number: ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter the second number: ");
+double b = Convert.ToDouble(Console.ReadLine());
+
+double c = ClassHw1.Qweqwe(a, b);
+Console.WriteLine($"Answer: {c}");
+
+// 3 задача
+Console.Write("Enter the first number: ");
+string r = Console.ReadLine();
+Console.Write("Enter the second number: ");
+string q = Console.ReadLine();
+ClassHw1.Third(ref r, ref q);
+Console.WriteLine($"{r}\n{q}");
+
+// 4 задача
+Console.Write("Введите число a, не равное нулю: ");
+double a_4 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите число b, не равное нулю: ");
+double b_4 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите число c, не равное нулю: ");
+double c_4 = Convert.ToDouble(Console.ReadLine());
+
+double x_4 = ClassHw1.LinearEquation(a_4, b_4, c_4);
+Console.WriteLine($"Ответ: {x_4}");
+
+// 1 задача по if
+Console.Write("Введите первое число: ");
+double a_if = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите второе число: ");
+double b_if = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine(ClassHw1.ExecuteIf(a_if, b_if));
+
