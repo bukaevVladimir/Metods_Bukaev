d93ad1820f046141f5e0f7b2e2d0a6d3547dee05
Vladimir Bukaev <bukaevv0@gmail.com>
2022-03-15 23:30:06 +0300

    Uncommented commented out  in HW1

--- HW_1/Program.cs
+++ HW_1/Program.cs
@@ -1,65 +1,65 @@
 ﻿using HW_1;
 
-////Пользователь вводит 2 числа (A и B). Выведите в консоль результат деления A на B и остаток от деления.
-//// 1 задача 
-////Console.Write("Enter the first number: ");
-////double a_1 = Convert.ToDouble(Console.ReadLine());
-////Console.Write("Enter the second number: ");
-////double b_1 = Convert.ToDouble(Console.ReadLine());
-
-
-
-
-//// 2 задача 
+//Пользователь вводит 2 числа (A и B). Выведите в консоль результат деления A на B и остаток от деления.
+// 1 задача 
 //Console.Write("Enter the first number: ");
-//double a = Convert.ToDouble(Console.ReadLine());
+//double a_1 = Convert.ToDouble(Console.ReadLine());
 //Console.Write("Enter the second number: ");
-//double b = Convert.ToDouble(Console.ReadLine());
+//double b_1 = Convert.ToDouble(Console.ReadLine());
 
-//double c = ClassHw1.Qweqwe(a, b);
-//Console.WriteLine($"Answer: {c}");
 
-//// 3 задача
-//Console.Write("Enter the first number: ");
-//string r = Console.ReadLine();
-//Console.Write("Enter the second number: ");
-//string q = Console.ReadLine();
 
-//ClassHw1.Third(ref r, ref q);
-//Console.WriteLine($"{r}\n{q}");
 
-//// 4 задача
-//Console.Write("Введите число a, не равное нулю: ");
-//double a_4 = Convert.ToDouble(Console.ReadLine());
-//Console.Write("Введите число b, не равное нулю: ");
-//double b_4 = Convert.ToDouble(Console.ReadLine());
-//Console.Write("Введите число c, не равное нулю: ");
-//double c_4 = Convert.ToDouble(Console.ReadLine());
+// 2 задача 
+Console.Write("Enter the first number: ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter the second number: ");
+double b = Convert.ToDouble(Console.ReadLine());
 
-//double x_4 = ClassHw1.LinearEquation(a_4, b_4, c_4);
-//Console.WriteLine($"Ответ: {x_4}");
+double c = ClassHw1.Qweqwe(a, b);
+Console.WriteLine($"Answer: {c}");
 
-//// 1 задача по if
-//Console.Write("Введите первое число: ");
-//int a_if = Convert.ToInt32(Console.ReadLine());
-//Console.Write("Введите второе число: ");
-//int b_if = Convert.ToInt32(Console.ReadLine());
+// 3 задача
+Console.Write("Enter the first number: ");
+string r = Console.ReadLine();
+Console.Write("Enter the second number: ");
+string q = Console.ReadLine();
 
-//Console.WriteLine(Homework2.ExecuteIf(a_if, b_if));
+ClassHw1.Third(ref r, ref q);
+Console.WriteLine($"{r}\n{q}");
 
-//// 2 задача по if
-//Console.Write("Введите x: ");
-//double x = Convert.ToDouble(Console.ReadLine());
-//Console.Write("Введите y: ");
-//double y = Convert.ToDouble(Console.ReadLine());
+// 4 задача
+Console.Write("Введите число a, не равное нулю: ");
+double a_4 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите число b, не равное нулю: ");
+double b_4 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите число c, не равное нулю: ");
+double c_4 = Convert.ToDouble(Console.ReadLine());
 
-//Console.WriteLine($"Точка лежит в {Homework2.QuarteroCordinates(x, y)} четверти");
+double x_4 = ClassHw1.LinearEquation(a_4, b_4, c_4);
+Console.WriteLine($"Ответ: {x_4}");
 
-//// 5 задача по if
-//Console.Write("Введите двузначное число: ");
-//int num = Convert.ToInt32(Console.ReadLine());
-//string result = ClassHw1.NumbersInLetters(num);
-//Console.WriteLine($"{result}");
+// 1 задача по if
+Console.Write("Введите первое число: ");
+int a_if = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b_if = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Homework2.ExecuteIf(a_if, b_if));
+
+// 2 задача по if
+Console.Write("Введите x: ");
+double x = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите y: ");
+double y = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine($"Точка лежит в {Homework2.QuarteroCordinates(x, y)} четверти");
+
+// 5 задача по if
+Console.Write("Введите двузначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+string result = ClassHw1.NumbersInLetters(num);
+Console.WriteLine($"{result}");
 
 // Задачи по циклам
 // 1 задача
@@ -72,18 +72,18 @@ int res = ClassForTasksByCycles.FirstDegree(number, degree);
 Console.WriteLine($"Ответ: {res}");
 
 
-//// 3 задача по циклам
-//int A;
-//do
-//{
-//    Console.Write("Введите число A: ");
-//    A = Convert.ToInt32(Console.ReadLine());
-//}
-//while (A <= 0);
+// 3 задача по циклам
+int A;
+do
+{
+    Console.Write("Введите число A: ");
+    A = Convert.ToInt32(Console.ReadLine());
+}
+while (A <= 0);
 
-//int c = 1;
-//while (Math.Pow(c, 2) < A)
-//{
-//    c++;
-//}
-//Console.WriteLine(c);
+int c1 = 1;
+while (Math.Pow(c, 2) < A)
+{
+    c++;
+}
+Console.WriteLine(c);
