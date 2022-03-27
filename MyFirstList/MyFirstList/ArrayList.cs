188418464618ff4ba0d2261773519dd7d58c3cba
Vladimir Bukaev <bukaevv0@gmail.com>
2022-03-27 20:09:35 +0300

    AddToStart

--- MyFirstList/MyFirstList/ArrayList.cs
+++ MyFirstList/MyFirstList/ArrayList.cs
@@ -6,71 +6,131 @@ using System.Threading.Tasks;
 
 namespace MyFirstList
 {
+    
     public class ArrayList
     {
-        public int Length { get; private set; }
-
-        public int this[int index]
-
-        {
-            get
-            {
-                if (index < 0 || index >= Length)
-                {
-                    throw new IndexOutOfRangeException();
-                }
-                return _array[index];
-            }
-            set
-            {
-                if (index < 0 || index >= Length)
-                {
-                    throw new IndexOutOfRangeException();
-
-                }
-                _array[index] = value;
-            }
-        }
-
-
         private int[] _array;
+        private int index;
 
+        public int Lenght { get; private set; }
         public ArrayList()
         {
-            _array = new int[10];
-            Length = 0;
+            _array = new int[5];
         }
 
         public void Add(int value)
         {
-            if (Length >= _array.Length)
+            if(!(Lenght < _array.Length))      
             {
-                UpSize();
+                UpSize();                
             }
-            _array[Length] = value;
-            Length++;
+            _array[Lenght] = value;
+            Lenght++;
+
         }
 
-        public void Write()
+       public void AddToStart(int value)
         {
-            for (int i = 0; i < Length; i++)
+            if(Lenght + 1 >= _array.Length)
             {
-                Console.Write($"{_array[i]} ");
+                int newLength = (int)(_array.Length * 1.5) + 1;
+                int[] newArray = new int[newLength];
+                Copy(newArray);
             }
-            Console.WriteLine();
+            int[] tttArray = new int[Lenght + 1];
+            for(int i = 0; i < index; i++)
+            {
+                tttArray[i] = _array[i];
+            }
+            for(int i = index; i < Lenght; i++)
+            {
+                tttArray[i + 1] = _array[i];
+            }
+            _array = tttArray;
+        }
+
+        private void Copy(int[] newArray)
+        {
+            throw new NotImplementedException();
         }
 
         private void UpSize()
         {
-            int newLength = (int)(_array.Length * 1.5d + 1);
-            int[] newArray = new int[newLength];
+            int newArrayLenght = (int)(_array.Length * 1.5d + 1);
+            int[] newArray = new int[newArrayLenght];
             for (int i = 0; i < _array.Length; i++)
             {
                 newArray[i] = _array[i];
             }
             _array = newArray;
-
         }
 
     }
+
+    //{
+    //    public int Length { get; private set; }
+
+    //    public int this[int index]
+
+    //    {
+    //        get
+    //        {
+    //            if (index < 0 || index >= Length)
+    //            {
+    //                throw new IndexOutOfRangeException();
+    //            }
+    //            return _array[index];
+    //        }
+    //        set
+    //        {
+    //            if (index < 0 || index >= Length)
+    //            {
+    //                throw new IndexOutOfRangeException();
+
+    //            }
+    //            _array[index] = value;
+    //        }
+    //    }
+
+
+    //    private int[] _array;
+
+    //    public ArrayList()
+    //    {
+    //        _array = new int[10];
+    //        Length = 0;
+    //    }
+
+    //    public void Add(int value)
+    //    {
+    //        if (Length >= _array.Length)
+    //        {
+    //            UpSize();
+    //        }
+    //        _array[Length] = value;
+    //        Length++;
+    //    }
+
+    //    public void Write()
+    //    {
+    //        for (int i = 0; i < Length; i++)
+    //        {
+    //            Console.Write($"{_array[i]} ");
+    //        }
+    //        Console.WriteLine();
+    //    }
+
+    //    private void UpSize()
+    //    {
+    //        int newLength = (int)(_array.Length * 1.5d + 1);
+    //        int[] newArray = new int[newLength];
+    //        for (int i = 0; i < _array.Length; i++)
+    //        {
+    //            newArray[i] = _array[i];
+    //        }
+    //        _array = newArray;
+
+    //    }
+
+    //}
 }
--- MyFirstList/MyFirstList/Program.cs
+++ MyFirstList/MyFirstList/Program.cs
@@ -1,8 +1,22 @@
 ﻿using MyFirstList;
 
-ArrayList list = new ArrayList();
-for(int i = 0; i < 10; i++)
-{
-    list.Add(i);
-    list.Write();
-}
+ArrayList MyArrayList = new ArrayList();
+Console.WriteLine(MyArrayList.Lenght);
+MyArrayList.Add(1);
+Console.WriteLine(MyArrayList.Lenght);
+//MyArrayList.Add(6);
+//Console.WriteLine(MyArrayList.Lenght);
+//MyArrayList.Add(65);
+//Console.WriteLine(MyArrayList.Lenght);
+//MyArrayList.AddToStart(1);
+//Console.WriteLine(MyArrayList.Lenght);
+
+
+//Car Lada = new Car("Lada");
+//Console.WriteLine(Lada.Model);
+//Lada.Drive();
+
+//Car Mersedes = new Car("Mersedes");
+//Console.WriteLine(Mersedes.Model);
+//Mersedes.Drive();
+
