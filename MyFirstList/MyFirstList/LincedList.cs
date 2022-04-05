3c20933e04e910292dfa186627f695ccd3c10806
Vladimir Bukaev <bukaevv0@gmail.com>
2022-04-05 18:29:27 +0300

    Worked on 2 tasks in LinkedList

--- MyFirstList/MyFirstList/LincedList.cs
+++ MyFirstList/MyFirstList/LincedList.cs
@@ -8,25 +8,94 @@ namespace MyFirstList
 {
     public class LincedList
     {
-        public int Length { get; private set; }
-
         private Node _root;
+
+        public int this[int index]
+        {
+            get
+            {
+                Node crnt = _root;
+                for (int i = 1; i <= index; i++)
+                {
+                    crnt = crnt.Next;
+                }
+                return crnt.Value;
+            }
+            set
+            {
+
+            }
+        }
+        public int Length
+        {
+            get
+            {
+                int count = 0;
+
+                Node crnt = _root;
+
+                while (crnt!= null)
+                {
+                    crnt = crnt.Next;
+                    count++;
+                }
+                return count;
+            }
+            private set
+            {
+
+            }
+        }
+
 
         public LincedList()
         {
-            Length = 0;
             _root = null;
         }
 
         public LincedList(int value)
         {
-            Length = 1;
             _root = new Node(value);
         }
 
-        public Add(int value)
+        public void Add(int value)
         {
-            Node crnt = _root;
+            if (_root == null)
+            {
+                _root = new Node(value);
+            }
+            else
+            {
+                Node crnt = _root;
+
+                while (crnt.Next != null)
+                {
+                    crnt = crnt.Next;
+                }
+
+                crnt.Next = new Node(value);
+
+            }
+        }
+        public void DeleteByIndex(int index)
+        {
+            if (index < 0 || index > Length - 1)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            else if (index == 0)
+            {
+                _root = _root.Next;
+            }
+            else
+            {
+                Node crnt = _root;
+                for (int i = 1; i < index; i++)
+                {
+                    crnt = crnt.Next;
+                }
+                crnt.Next = crnt.Next.Next;
+            }
         }
 
     }
